55e9a65fe9eef4ebfdfdfdf9be4d5dfee875ab40
Thushalya <weeeratharava@gmail.com>
2022-11-04 07:49:30 +0530

    initial commit

---
+++ src/main/java/com/findyourworker/findyourworker/FindyourworkerApplication.java
@@ -0,0 +1,13 @@
+package com.findyourworker.findyourworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindyourworkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindyourworkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/findyourworker/findyourworker/FindyourworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findyourworker.findyourworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindyourworkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
